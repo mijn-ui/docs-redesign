a58ab9c34c2caabedc8f6aef9bdd7183bf9e35bc
HTLA380 <htetlin099798@gmail.com>
2025-12-09 17:24:21 +0630

    feat: improve seo stuffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
 import "./global.css";
 import { Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next";
+import { baseUrl, createMetadata } from "@/lib/metadata";
 import { FumadocsRootProvider } from "../components/fumadocs-root-provider";
 
 const inter = Inter({
   subsets: ["latin"],
+});
+
+export const metadata = createMetadata({
+  title: {
+    template: "%s | MijnUI",
+    default: "MijnUI",
+  },
+  description:
+    "MijnUI offers flexible, ready-to-use components for building marketing sites, dashboards, and e-commerce platforms.",
+  metadataBase: baseUrl,
 });
 
 export default function Layout({ children }: LayoutProps<"/">) {
--- src/app/og/docs/[...slug]/route.tsx
+++ src/app/og/docs/[...slug]/route.tsx
@@ -16,7 +16,7 @@ export async function GET(
   return new ImageResponse(
     <DefaultImage
       description={page.data.description}
-      site="My App"
+      site="mijnui"
       title={page.data.title}
     />,
     {
